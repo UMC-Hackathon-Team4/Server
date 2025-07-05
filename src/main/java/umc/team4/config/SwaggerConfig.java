a445e9b68830f3067ed8a59fd456d44606e3e9df
Claire <113418806+Sunho12@users.noreply.github.com>
2025-07-06 08:53:45 +0900

    Update SwaggerConfig.java

--- src/main/java/umc/team4/config/SwaggerConfig.java
+++ src/main/java/umc/team4/config/SwaggerConfig.java
@@ -21,10 +21,14 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("함:께 - 장애인 창작자 프로젝트 플랫폼 API")
-                        .description("""
+
+        Server httpsServer = new Server();
+        httpsServer.setUrl("https://togetherumc.kro.kr");
+        httpsServer.setDescription("Production HTTPS Server");
+
+        Info apiInfo = new Info()
+                .title("함:께 - 장애인 창작자 프로젝트 플랫폼 API")
+                .description("""
                         장애인과 비장애인이 함께하는 창작 프로젝트 플랫폼 '함:께'의 OpenAPI 문서입니다.
 
                         ✅ 주요 기능:
@@ -32,9 +36,11 @@ public class SwaggerConfig {
                         - 전체/카테고리별 프로젝트 조회
                         - 프로젝트 상세 조회
                         - 함께하기(펀딩하기)
-
                         """)
-                        .version("1.0.0"));
-    }
+                .version("1.0.0");
 
+        return new OpenAPI()
+                .servers(List.of(httpsServer))
+                .info(apiInfo);
+    }
 }
